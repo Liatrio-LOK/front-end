d2a73df776e9e89c3a4cae1f2af6696d8d676d89
Dmitry <dmitry@liatrio.com>
2018-02-01 14:17:35 -0800

    clean up

--- front-end-test-grid.rb
+++ front-end-test-grid.rb
@@ -1,31 +1,27 @@
-#require 'headless'
 require 'selenium-webdriver'
 require 'minitest/autorun'
 
 describe 'Sock-Shop' do
   before do
-    #@headless = Headless.new
-    #@headless.start
-
     @driver = Selenium::WebDriver.for(
-      :remote,
-      url: 'http://standalone-firefox:4444/wd/hub',
-      desired_capabilities: :firefox) # look into desired_capabilities. browserName?
+        :remote,
+        url: 'http://standalone-firefox:4444/wd/hub',
+        desired_capabilities: :firefox)
     @driver.navigate.to 'http://front-end:8079'
     #@driver.manage.timeouts.implicit_wait = 30
   end
 
   after do
-    #@headless.destroy
     @driver.quit
   end
 
   describe 'test title' do
     it 'should show WeaveSocks title' do
-        puts @driver.title
+      puts @driver.title
       assert @driver.title == 'WeaveSocks'
     end
   end
+
   describe 'test title' do
     it 'should not show Google as the title' do
       assert @driver.title == 'Google'
