d5ed80d8f4b95d084817bc245479d93d108d947a
Jeff Guzman <jeff@liatrio.com>
2018-02-14 14:26:13 -0800

    adding webdriver

--- test/cucumber/step_definitions/home_page.js
+++ test/cucumber/step_definitions/home_page.js
@@ -1,12 +1,19 @@
   'use strict';
   var {defineSupportCode} = require('cucumber');
+  var  webdriver = require('selenium-webdriver');
   var assert = require('assert');
+
+  var driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
 
   defineSupportCode(function({Given, Then, When}) {
 
     Given(/^I navigate to Sock Shop web page$/, function () {
+      driver.get('https://www.google.com/');
     });
 
     When(/^I should see WeaveSocks page title$/, function () {
+      driver.findElement(webdriver.By.name('title'));
     });
   });
