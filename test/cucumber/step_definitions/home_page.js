a500f41aa7807cb70c2b59e323351e0bdb58c937
Jeff Guzman <jeff@liatrio.com>
2018-02-14 16:06:12 -0800

    adding selenium test

--- test/cucumber/step_definitions/home_page.js
+++ test/cucumber/step_definitions/home_page.js
@@ -2,9 +2,12 @@
   var {defineSupportCode} = require('cucumber');
   var  webdriver = require('selenium-webdriver');
   var assert = require('assert');
+  var selenium = "http://" + process.argv[5] + ":30002/wd/hub"
+
 
   var driver = new webdriver.Builder()
       .forBrowser('chrome')
+      .usingServer(selenium)
       .build();
 
   defineSupportCode(function({Given, Then, When}) {
@@ -15,5 +18,6 @@
 
     When(/^I should see WeaveSocks page title$/, function () {
       driver.findElement(webdriver.By.name('title'));
+      console.log(driver.getTitle());
     });
   });
